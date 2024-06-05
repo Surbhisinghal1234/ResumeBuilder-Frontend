cd69b3862ea1dae37ad25f893c7e22936f23a86e
Surbhisinghal1234 <surbhi98singhal@gmail.com>
2024-06-05 09:01:42 +0530

    change routes

--- src/Resume/Builder.jsx
+++ src/Resume/Builder.jsx
@@ -11,32 +11,32 @@ function Builder() {
       <div className="md:h-screen w-full md:w-[20%] py-[2rem] md:p-4 bg-slate-200 flex flex-col items-center md:items-stretch gap-[1.5rem] md:gap-[1rem] ">
         <div className="">
           
-           <NavLink className="text-3xl md:text-2xl font-bold" to="/create">Resume Builder</NavLink>
+           <Link className="text-3xl md:text-2xl font-bold" to="/create">Resume Builder</Link>
         </div>
         <ul className="flex flex-col gap-[1.6rem] ">
           <li className="w-[20rem] md:w-auto ">
-            <NavLink to="my-details" className=" flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-2 py-3 md:py-2  rounded-md bg-slate-700 text-white">
+            <Link to="my-details" className=" flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-2 py-3 md:py-2  rounded-md bg-slate-700 text-white">
               <BadgeIcon className="mr-2" />
               My Details
-            </NavLink>
+            </Link>
           </li>
           <li>
-            <NavLink  to="about-me" className="flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-2 py-3 md:py-2  rounded-md bg-slate-700 text-white">
+            <Link  to="about-me" className="flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-2 py-3 md:py-2  rounded-md bg-slate-700 text-white">
               <InfoIcon className="mr-2" />
               About Me
-            </NavLink>
+            </Link>
           </li>
           <li>
-            <NavLink to="skills-and-proficiencies" className="flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-4 py-3 md:py-2 md:px-2  rounded-md bg-slate-700 text-white">
+            <Link to="skills-and-proficiencies" className="flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-4 py-3 md:py-2 md:px-2  rounded-md bg-slate-700 text-white">
               <MiscellaneousServicesIcon className="mr-2"/>
              Skills & Proficiencies
-            </NavLink>
+            </Link>
           </li>
           <li>
-            <NavLink  to="work-experiences" className="flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-2 py-3 md:py-2 rounded-md bg-slate-700 text-white">
+            <Link  to="work-experiences" className="flex items-center text-[1.2rem] gap-x-[1rem] md:text-[1rem] md:gap-x-0 w-full inline-block px-2 py-3 md:py-2 rounded-md bg-slate-700 text-white">
               <BusinessCenterOutlinedIcon className="mr-2" />
               Work Exprience
-            </NavLink>
+            </Link>
           </li>
         </ul>
       </div>
